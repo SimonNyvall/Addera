f8144f848bf4a2684d2e6d6709f85d7a102b6645
Simon Nyvall <50596493+SimonNyvall@users.noreply.github.com>
2019-05-14 18:41:02 +0200

    Add files via upload

---
+++ Addera.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Nummer: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Addera med: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("svar: " + (num1 + num2 ));
+
+            Console.ReadLine();
+        }
+    }
+}
